22bfe4fa07b0976288dddd681765f025acc6ca36
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2023-05-27 23:03:12 -0400

    Added UI and fix DB access issue.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,28 +1,37 @@
 import AWS from 'aws-sdk';
 import type { ScanInput } from 'aws-sdk/clients/dynamodb.js';
 
+export interface OilPriceData {
+	date: string;
+	price: number;
+	gallons: number;
+}
+
 export async function load({ params }) {
 	const awsConfig = {
 		region: 'us-east-1',
-		endpoint: 'http://dynamodb.us-east-1.amazonaws.com'
+		endpoint: 'http://dynamodb.us-east-1.amazonaws.com',
+		accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
+		secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY
 	};
 	AWS.config.update(awsConfig);
 
 	const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 	const dbParams: ScanInput = {
-		TableName: 'OilPrices'
+		TableName: 'Oil-Price'
 	};
 
-	const scanResults: unknown[] = [];
-	let items;
+	const scanResults: OilPriceData[] = [];
 
-	do {
-		console.log('Getting data from dynamo: ');
-		items = await dynamodb.scan(dbParams).promise();
-		items.Items?.forEach((item) => scanResults.push(item));
-		dbParams.ExclusiveStartKey = items.LastEvaluatedKey;
-	} while (typeof items.LastEvaluatedKey != 'undefined');
+	console.log('Getting data from dynamo: ');
+	const items = await dynamodb.scan(dbParams).promise();
+	console.log('result from db: ', items);
 
-	return scanResults;
+	items.Items?.forEach((item) => scanResults.push(item as OilPriceData));
+	dbParams.ExclusiveStartKey = items.LastEvaluatedKey;
+
+	scanResults.sort((a, b) => (a.date < b.date ? 1 : -1));
+	console.log('Done parsing data: ', scanResults);
+	return { scanData: scanResults };
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,52 @@
-<script>
-  export let data;
+<script lang="ts">
+	import type { OilPriceData } from './+page.server.js';
+
+  export let data: {scanData: OilPriceData[]};
+  function formatDate(dateString: string) {
+    const date = new Date(dateString);
+    const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' } as Intl.DateTimeFormatOptions;
+    return date.toLocaleDateString('en-us', options);
+  }
+
+  function groupByDate(data: OilPriceData[]) {
+    const groupedData: {[key: string]: { date: string, prices: OilPriceData[] }} = {};
+    data.forEach((item: OilPriceData) => {
+      const date = formatDate(item.date);
+      if (!groupedData[date]) {
+        groupedData[date] = { date, prices: [] };
+      }
+      groupedData[date].prices.push(item);
+    });
+    return Object.values(groupedData);
+  }
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>{data.gallon}</p>
-<p>{data.price}</p>
+<div class="bg-gray-900 min-h-screen">
+  <nav class="bg-gray-800 p-4">
+    <h1 class="text-white text-2xl">Welcome to SvelteKit</h1>
+    <p class="text-white">Visit <a href="https://kit.svelte.dev" class="text-blue-500">kit.svelte.dev</a> to read the documentation</p>
+  </nav>
+
+  <div class="container mx-auto py-8">
+    <h2 class="text-white text-2xl mb-4">Oil Price Data</h2>
+
+    {#each groupByDate(data.scanData) as { date, prices }}
+      <div class="mb-4">
+        <h3 class="text-white text-lg mb-2">Date: {date}</h3>
+
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {#each prices as item}
+            <div class="bg-gray-700 p-4 rounded-lg">
+              <p class="text-white">Gallons: {item.gallons}</p>
+              <p class="text-white">Price: {item.price}</p>
+            </div>
+          {/each}
+        </div>
+      </div>
+    {/each}
+
+    {#if !data || data.scanData.length === 0}
+      <p class="text-white">No data available</p>
+    {/if}
+  </div>
+</div>
