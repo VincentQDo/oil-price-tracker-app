3e5a1cc26ea260974be3fec31fc04a814e126943
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2023-06-03 18:53:20 -0400

    fix style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 	export let data: { oilData: GroupedDataItem[] };
 </script>
 
-<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
 	{#each data.oilData as supplierData}
 		<div>
 			<PriceCard data={supplierData} />
