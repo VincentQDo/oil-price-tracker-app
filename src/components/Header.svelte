ce21cea04f62673053f1357059c8e9985099107c
Vince <vincentquangdo@gmail.com>
2023-06-24 17:13:13 -0400

    clean up header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,8 +1,9 @@
 <nav class="flex items-center justify-between bg-indigo-800 text-white p-4">
-	<div class="text-2xl">Oil Tracker</div>
-	<div class="justify-center flex items-center space-x-4">
-		<a href="/" class="text-white hover:underline">Home</a>
-		<a href="/about" class="text-white hover:underline">About</a>
+	<div class="text-2xl">
+		<a href="/">Oil Tracker</a>
 	</div>
-	<button class="text-white hover:underline">Login</button>
+	<div class="justify-center flex items-center space-x-4">
+		<!-- Middle nav links here -->
+	</div>
+	<!-- <button class="text-white hover:underline">Login</button> -->
 </nav>
