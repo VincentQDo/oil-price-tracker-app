2413e158290a659ed0157edbf25ddf818480fd44
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2023-06-25 10:35:31 -0400

    Update page title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,5 +3,10 @@
 	import Header from '../components/Header.svelte';
 </script>
 
+<svelte:head>
+	<title>Oil Price Tracker</title>
+	<meta name="description" content="This is where the description goes for SEO" />
+</svelte:head>
+
 <Header />
 <slot />
