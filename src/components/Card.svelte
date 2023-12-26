679dfdc0d0f7341f24424e9a91f600d666e8f6f7
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2023-12-25 20:28:52 -0500

    Update text styles and allow supplier name prefix

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -6,9 +6,9 @@
 <div
 	class="rounded-2xl bg-gray-900 ring-1 ring-inset ring-gray-100/5 lg:flex lg:flex-col lg:justify-center"
 >
-	<div class="py-5 text-center">
-		<h1 class="text-4xl pb-4 font-bold tracking-tight text-white">{title}</h1>
-		<h2 class="text-3xl font-bold tracking-tight text-white">{subtitle}</h2>
+	<div class="pt-5 pb-2 text-center">
+		<h1 class="text-2xl pb-2 font-bold tracking-tight text-white">{title}</h1>
+		<h2 class="text-xl font-bold tracking-tight text-white">{subtitle}</h2>
 	</div>
 	<slot name="content" />
 	<slot name="footer" />
--- src/components/PricePerGallonText.svelte
+++ src/components/PricePerGallonText.svelte
@@ -2,13 +2,15 @@
 	import type { OilPriceData } from '../interfaces/OilPrices';
 
 	export let data: OilPriceData;
+	export let supplier: string = '';
 </script>
 
 <div class="mx-auto max-w-xs px-8">
-	<p class="mt-6 flex items-baseline justify-center gap-x-2">
-		<span class="text-4xl font-bold tracking-tight text-white"
-			>${data.price.toFixed(2)}/{data.gallons}</span
-		>
-		<span class="text-sm font-semibold leading-6 tracking-wide text-gray-400">USD/Gallons</span>
+	<p class="mt-2 flex items-baseline justify-center gap-x-2">
+		{#if supplier}
+			<span class="text-l font-bold tracking-tight text-white">{supplier}:</span>
+		{/if}
+		<span class="text-xl font-bold tracking-tight text-white">${data.price.toFixed(2)}</span>
+		<span class="text-sm font-semibold leading-6 tracking-wide text-gray-400">/Gallons</span>
 	</p>
 </div>
